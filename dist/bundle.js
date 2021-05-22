5ea98d0a608dfc094cd03fb04a4e659a87096c81
lukas-schmid <schmid.luk@hotmail.com>
2021-05-22 13:19:19 +0200

    added browserlist to package.json

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,7 +6,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
@@ -14,7 +14,7 @@
 /*!*********************************!*\
   !*** ./src/javascript/index.js ***!
   \*********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/styles.scss */ \"./src/styles/styles.scss\");\n\n\nwindow.onscroll = function () {\n  showNavbar();\n  elementOpacity(\"#top\");\n  elementOpacity(\"#projects\");\n  elementOpacity(\"#contact\");\n};\n\nvar showNavbar = function showNavbar() {\n  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {\n    document.querySelector(\".navbar\").style.top = \"0\";\n  } else {\n    document.querySelector(\".navbar\").style.top = \"-50px\";\n  }\n};\n\nvar elementOpacity = function elementOpacity(element) {\n  var position = document.querySelector(element).getBoundingClientRect();\n  var scrolledFromTopInPercentage = Math.abs(position.y) * 100 / position.height;\n  var opacityValue = (100 - scrolledFromTopInPercentage) / 100;\n  document.querySelector(element).style.opacity = opacityValue;\n};\n\n//# sourceURL=webpack://webpack-portfolio/./src/javascript/index.js?");
 
@@ -24,7 +24,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
 /*!********************************!*\
   !*** ./src/styles/styles.scss ***!
   \********************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-portfolio/./src/styles/styles.scss?");
 
@@ -58,15 +58,15 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
+/******/ 		__webpack_require__.r = function(exports) {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /************************************************************************/
 /******/ 	
