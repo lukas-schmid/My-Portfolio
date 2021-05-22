5cec03b94c72068f90480f48767edc7e715a6e25
lukas-schmid <schmid.luk@hotmail.com>
2021-05-22 16:36:32 +0200

    navbar design changes

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -11,7 +11,7 @@ const showNavbar = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.querySelector(".navbar").style.top = "0";
   } else {
-    document.querySelector(".navbar").style.top = "-50px";
+    document.querySelector(".navbar").style.top = "-75px";
   }
 };
 
