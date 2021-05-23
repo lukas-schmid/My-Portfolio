02916af64d8a93dbd9a1558c9e8fbb7787bb35ff
lukas-schmid <schmid.luk@hotmail.com>
2021-05-23 22:50:03 +0200

    navbar script

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,17 @@
 import "../styles/styles.scss";
 
+window.onscroll = function () {
+  handleNavbarOpacity();
+};
+
+const handleNavbarOpacity = () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY <= 60) {
+    const opacity = (window.scrollY * 100) / 60 / 100;
+    navbar.style.backgroundColor = "rgba(228,0,0," + opacity + ")";
+  }
+};
+
 const openMobileMenu = () => {
   const navbarIcon = document.querySelector(".navbar__icon");
   const mobileMenu = document.querySelector(".navbar__mobile");
